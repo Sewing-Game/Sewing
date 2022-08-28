905b8d549bb8ae1e910f2fdb48fb0c0753191107
mini102 <mooya1024@ajou.ac.kr>
2022-08-28 16:58:01 +0900

    npc generation & move

---
+++ Assets/script/CameraLogic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLogic : MonoBehaviour
+{
+
+    private Transform m_currentTarget = null;
+    private float m_distance = 2f;
+    private float m_height = 1;
+    private float m_lookAtAroundAngle = 180;
+
+    [SerializeField] private List<Transform> m_targets = null;
+    [SerializeField] private MeshFilter planeMesh = null;
+    private Bounds bounds;
+    private float boundsX = 0f;
+    private float boundsZ = 0f; 
+    private int m_currentIndex = 0;
+
+    private void Start()
+    {
+        if (m_targets.Count > 0)
+        {
+            m_currentIndex = 0;
+            m_currentTarget = m_targets[m_currentIndex];
+        }
+        Mesh meshy = planeMesh.mesh;
+        bounds = meshy.bounds;
+        boundsX = (bounds.size.x)/2;
+        boundsZ =(bounds.size.z)/2;
+        float x = Random.Range(-boundsX,boundsX);
+        float z = Random.Range(-boundsZ,boundsZ);
+        m_currentTarget.position = new Vector3(x,m_currentTarget.position.y,z);
+    }
+
+    private void LateUpdate()
+    {
+        if (m_currentTarget == null) { return; }
+        transform.LookAt(m_currentTarget.position + new Vector3(0, m_height, 0));
+    }
+}
---
+++ Assets/script/NPCManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour
+{
+    [SerializeField] private float m_moveSpeed = 0.1f;
+    [SerializeField] private Transform destination = null;
+
+    private List<Collider> m_collisions = new List<Collider>();
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint[] contactPoints = collision.contacts;
+        for (int i = 0; i < contactPoints.Length; i++)
+        {
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+            {
+                if (!m_collisions.Contains(collision.collider))
+                {
+                    m_collisions.Add(collision.collider);
+                }
+            }
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        ContactPoint[] contactPoints = collision.contacts;
+        bool validSurfaceNormal = false;
+        for (int i = 0; i < contactPoints.Length; i++)
+        {
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+            {
+                validSurfaceNormal = true; break;
+            }
+        }
+
+        if (validSurfaceNormal)
+        {
+            if (!m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Add(collision.collider);
+            }
+        }
+        else
+        {
+            if (m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Remove(collision.collider);
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (m_collisions.Contains(collision.collider))
+        {
+            m_collisions.Remove(collision.collider);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        DirectUpdate();
+    }
+
+    private void DirectUpdate()
+    {
+
+        Transform camera = Camera.main.transform;
+
+        Vector3 des = destination.position;
+
+        if (des != Vector3.zero)
+        {
+            //transform.LookAt(des);
+            des = new Vector3(des.x,transform.position.y,des.z);
+            transform.position = Vector3.MoveTowards(transform.position, des, m_moveSpeed * Time.deltaTime);
+        }
+    }
+}
