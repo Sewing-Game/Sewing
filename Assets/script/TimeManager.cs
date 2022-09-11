85cfd8619e5ed49463b0daca9227f690712a01a0
Chaemin <73246476+mini102@users.noreply.github.com>
2022-09-11 16:48:42 +0900

    Update TimeManager.cs
    rearrangement

--- Assets/script/TimeManager.cs
+++ Assets/script/TimeManager.cs
@@ -6,19 +6,21 @@ using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
-    public const int monthByYear = 12;
-    public const int monthlyDate = 30;  
+    private const int monthByYear = 12;
+    private const int monthlyDate = 30;  
 
-    public const long daySecond = 24 * 60 * 60;  //하루간의 총 초
-    public const long monthSecond =  monthlyDate * daySecond; //한 달간의 총 초
-    public const long yearSecond = monthByYear * monthSecond; //1년간의 총 초
-    private int inityear=102;
-    private int initmonth=4;
-    private int initday=1;
-    //[Range(1,300)]
+    private const long daySecond = 24 * 60 * 60;  //total seconds of a day
+    private const long monthSecond =  monthlyDate * daySecond; //total seconds of a month
+    private const long yearSecond = monthByYear * monthSecond; //total seconds of a year
+    public int inityear=102;
+    public int initmonth=4;
+    public int initday=1;
     public float speed = 96f;
-
-    public float _totalTime;
+    private int tmp=0;
+    private int year=0;
+    private int month=0;
+    private int day=0;
+    private float _totalTime;
     private NemoDate date;
     public int Year => date.year;
     public int Month => date.month;
@@ -30,18 +32,24 @@ public class TimeManager : MonoBehaviour
     private void Start()
     {
         Time.timeScale = speed;
-        date = new NemoDate();
+        date = new NemoDate(inityear,initmonth,initday);
     }
 
     void Update()
     {
-        _totalTime += Time.deltaTime * Time.timeScale *speed;
+        _totalTime += Time.deltaTime * Time.timeScale;
         float t = _totalTime;
-        date.year = (int)(t / yearSecond);
+        tmp = year;
+        year = (int)(t / yearSecond);
+        if (tmp!=year) date.year+=1;
         t %= yearSecond;
-        date.month = (int)(t / monthSecond);
+        tmp = month;
+        month = (int)(t / monthSecond);
+        if (tmp!=month) date.month+=1;
         t %= monthSecond;
-        date.day = (int)(t / daySecond);
+        tmp = day;
+        day = (int)(t / daySecond);
+        if (tmp!=day) date.day+=1;
         t %= daySecond;
         date.hour = (int)(t / 3600);
         t %= 3600;
